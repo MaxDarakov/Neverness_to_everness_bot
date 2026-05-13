33697dda9596580071b4037720338bec1ad4ce37
MaxDarakov <maksimys2320775@gmail.com>
2026-05-13 09:39:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,61 @@
+from telegram import ReplyKeyboardMarkup, Update
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
 import os
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TOKEN")
-WEBHOOK_URL = "https://neverness-to-everness-bot.onrender.com"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот працює через webhook! ✅")
 
-def main():
-    app = Application.builder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
+    keyboard = [
+        ["📌 Інфо", "🎮 Ігри"],
+        ["😂 Мем", "❌ Закрити"]
+    ]
 
-    # Запускаємо webhook замість polling
-    app.run_webhook(
-        listen="0.0.0.0",
-        port=int(os.getenv("PORT", 10000)),
-        url_path=TOKEN,
-        webhook_url=f"{WEBHOOK_URL}/{TOKEN}"
+    reply_markup = ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True
     )
 
-if __name__ == "__main__":
-    main()
+    await update.message.reply_text(
+        "Привіт! Я працюю через Render 😎",
+        reply_markup=reply_markup
+    )
+
+async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    text = update.message.text
+
+    if text == "📌 Інфо":
+        await update.message.reply_text(
+            "Я Telegram-бот."
+        )
+
+    elif text == "🎮 Ігри":
+        await update.message.reply_text(
+            "Minecraft, GTA V, Terraria"
+        )
+
+    elif text == "😂 Мем":
+        await update.message.reply_text(
+            "— Мамо, я програміст.\n— Тоді полагодь принтер."
+        )
+
+    elif text == "❌ Закрити":
+        await update.message.reply_text(
+            "Меню закрито."
+        )
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT, buttons))
+
+print("Бот запущений!")
+
+app.run_polling()
